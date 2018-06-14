81d0e4ff9d876e49dba193e431891de68b32d0e1
Mr.H <hyy8516998@163.com>
2018-06-14 09:35:57 +0800

    完成header和tab区域

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,28 @@
 <template>
-  <div>
+  <div id="app-container">
     <h1>这是 App 组件</h1>
+    <!--顶部Header区域-->
+    <mt-header fixed title="Vue --- SnailLemon"></mt-header>
 
-    
+    <!--底部tab区域-->
+    <nav class="mui-bar mui-bar-tab">
+      <a class="mui-tab-item mui-active" href="#tabbar">
+        <span class="mui-icon mui-icon-home"></span>
+        <span class="mui-tab-label">首页</span>
+      </a>
+      <a class="mui-tab-item" href="#tabbar-with-chat">
+        <span class="mui-icon mui-icon-email"><span class="mui-badge">9</span></span>
+        <span class="mui-tab-label">消息</span>
+      </a>
+      <a class="mui-tab-item" href="#tabbar-with-contact">
+        <span class="mui-icon mui-icon-contact"></span>
+        <span class="mui-tab-label">通讯录</span>
+      </a>
+      <a class="mui-tab-item" href="#tabbar-with-map">
+        <span class="mui-icon mui-icon-gear"></span>
+        <span class="mui-tab-label">设置</span>
+      </a>
+    </nav>
   </div>
 </template>
 
@@ -12,5 +32,7 @@
 
 
 <style scoped lang="scss">
-
+  #app-container {
+    padding-top: 40px;
+  }
 </style>
